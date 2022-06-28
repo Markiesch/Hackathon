ff0fcdf6b37bca958932add325c6c1ec2450a8a0
Mark Schuurmans <marksmeel@gmail.com>
2022-06-28 13:11:31 +0200

    Added admin panel

---
+++ pages/overview.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Sidebar />
+    <div>overview</div>
+  </div>
+</template>
+
+<style lang="scss">
+div {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  min-height: 100vh;
+
+  & > * {
+    height: 100%;
+  }
+}
+</style>
