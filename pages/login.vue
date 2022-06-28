80e0a2167183dd16408cb5565c4f5714af6e83ce
Mark Schuurmans <marksmeel@gmail.com>
2022-06-28 10:20:28 +0200

    Added login page

---
+++ pages/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <div>
+      <img src="/hero.jpg" alt="People celebrating together" />
+    </div>
+    <form>
+      <h1>Login</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam corporis voluptatem reprehenderit perspiciatis aspernatur ipsum quod esse earum veritatis, rem nam doloremque nostrum omnis
+        voluptates architecto. Voluptate officiis ipsam iste!
+      </p>
+      <label for="username">Gebruikersnaam</label>
+      <input type="text" id="username" />
+      <label for="password">Wachtwoord</label>
+      <input type="password" id="password" />
+      <button class="button--login" @click="login">Login</button>
+      <NuxtLink to="/" class="button--back">Terug</NuxtLink>
+    </form>
+  </section>
+</template>
+
+<script lang="ts" setup>
+function login() {
+  // TODO inloggen
+}
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/pages/login";
+</style>
