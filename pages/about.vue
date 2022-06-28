07821fefeefd9081f1de25caf890de61fc56f5e9
Mark Schuurmans <marksmeel@gmail.com>
2022-06-28 13:13:42 +0200

    Simplified markup

--- pages/about.vue
+++ pages/about.vue
@@ -1,27 +1,24 @@
 <template>
   <Navigation />
-  <section class="fotoSection">
-    <img src="/Medewerker-ICT-1280x810.jpg" />
+  <img src="/Medewerker-ICT-1280x810.jpg" />
 
-    <div class="container">
-      <h1 class="titel">Over Ons</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatibus deleniti cumque nemo magni reprehenderit odit ipsa quasi accusantium laudantium. Alias molestiae ad, labore
-        mollitia minima hic rem numquam quaerat quae sunt odit consectetur? Iure, expedita. Optio nobis provident quam, illum harum dolore veritatis, reiciendis quis, inventore numquam fuga dolorem.
-        Nesciunt sed aliquid nisi. Blanditiis tempora doloremque nobis magni fugiat earum aperiam culpa architecto illum neque, maiores ex pariatur labore? Officia omnis velit commodi vero mollitia,
-        libero id quam iusto aut praesentium ad! Itaque aliquid adipisci ratione aliquam dolorem tempore nobis iure, necessitatibus expedita error dolores velit harum, inventore earum? Omnis nisi
-        porro, corrupti architecto eum tempora animi facere, unde sed minima placeat velit nulla et voluptas asperiores. Accusantium modi eligendi ea eveniet optio maxime sunt at, facilis, dolor
-        dolorem neque nesciunt expedita, nemo quae quidem perspiciatis! Tempora laudantium, nemo exercitationem praesentium sit eveniet delectus eligendi nobis perspiciatis consequuntur laboriosam,
-        nihil rerum facere harum reiciendis, modi corporis iure. Inventore minus rem laboriosam officiis quod temporibus, adipisci repellat porro distinctio, esse nobis, doloribus dolorem itaque
-        deleniti. Officiis molestiae quo dicta impedit accusantium, beatae, voluptates, ipsam et minima fugit laboriosam excepturi cumque illum sed cupiditate itaque tenetur consequatur explicabo.
-        Quasi dicta reiciendis et totam sunt quibusdam laborum, facere qui magni sequi delectus dolor repellendus iure minus sed autem tempore ut veniam. Hic eligendi harum fugiat iure. Nulla earum
-        necessitatibus cum ipsam omnis accusamus odio aperiam illo accusantium perferendis similique vitae vero laboriosam voluptate facere, iste, itaque distinctio. Saepe libero aperiam, quidem, quae
-        error nulla provident aliquid quod suscipit ratione, recusandae nesciunt placeat reiciendis! Beatae harum excepturi ex provident quibusdam repellat blanditiis modi, aspernatur quidem et
-        facilis quam vel, dolor nam doloremque exercitationem ipsam non sint sit praesentium. Labore sit totam beatae obcaecati eveniet tempore asperiores dolorem architecto natus. Error adipisci
-        vitae voluptatum!
-      </p>
-    </div>
-  </section>
+  <div class="container">
+    <h1 class="titel">Over Ons</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatibus deleniti cumque nemo magni reprehenderit odit ipsa quasi accusantium laudantium. Alias molestiae ad, labore
+      mollitia minima hic rem numquam quaerat quae sunt odit consectetur? Iure, expedita. Optio nobis provident quam, illum harum dolore veritatis, reiciendis quis, inventore numquam fuga dolorem.
+      Nesciunt sed aliquid nisi. Blanditiis tempora doloremque nobis magni fugiat earum aperiam culpa architecto illum neque, maiores ex pariatur labore? Officia omnis velit commodi vero mollitia,
+      libero id quam iusto aut praesentium ad! Itaque aliquid adipisci ratione aliquam dolorem tempore nobis iure, necessitatibus expedita error dolores velit harum, inventore earum? Omnis nisi porro,
+      corrupti architecto eum tempora animi facere, unde sed minima placeat velit nulla et voluptas asperiores. Accusantium modi eligendi ea eveniet optio maxime sunt at, facilis, dolor dolorem neque
+      nesciunt expedita, nemo quae quidem perspiciatis! Tempora laudantium, nemo exercitationem praesentium sit eveniet delectus eligendi nobis perspiciatis consequuntur laboriosam, nihil rerum facere
+      harum reiciendis, modi corporis iure. Inventore minus rem laboriosam officiis quod temporibus, adipisci repellat porro distinctio, esse nobis, doloribus dolorem itaque deleniti. Officiis
+      molestiae quo dicta impedit accusantium, beatae, voluptates, ipsam et minima fugit laboriosam excepturi cumque illum sed cupiditate itaque tenetur consequatur explicabo. Quasi dicta reiciendis
+      et totam sunt quibusdam laborum, facere qui magni sequi delectus dolor repellendus iure minus sed autem tempore ut veniam. Hic eligendi harum fugiat iure. Nulla earum necessitatibus cum ipsam
+      omnis accusamus odio aperiam illo accusantium perferendis similique vitae vero laboriosam voluptate facere, iste, itaque distinctio. Saepe libero aperiam, quidem, quae error nulla provident
+      aliquid quod suscipit ratione, recusandae nesciunt placeat reiciendis! Beatae harum excepturi ex provident quibusdam repellat blanditiis modi, aspernatur quidem et facilis quam vel, dolor nam
+      doloremque exercitationem ipsam non sint sit praesentium. Labore sit totam beatae obcaecati eveniet tempore asperiores dolorem architecto natus. Error adipisci vitae voluptatum!
+    </p>
+  </div>
 </template>
 <style lang="scss">
 @use "@/styles/pages/about";
