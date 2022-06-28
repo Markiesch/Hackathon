b8c5778896a70b342912ce76b4774d9e79abb4bd
Markiesch <marksmeel@gmail.com>
2022-06-28 19:21:29 +0200

    Reworked hero section

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <Navigation></Navigation>
-    <section>
+    <section class="hero">
       <div>
         <h1>Samen tegen eenzaamheid</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ex tempora qui. <br />
           Velit architecto, deserunt, autem porro, dolor provident jatoch
         </p>
-        <div>
-          <NuxtLink to="/overview" class="cta--overview">Overzicht</NuxtLink>
-          <NuxtLink to="/about" class="cta--learn-more">Learn More</NuxtLink>
-        </div>
+        <NuxtLink to="/overview" class="cta--overview">Overzicht</NuxtLink>
       </div>
-
-      <div>
-        <img src="/hero.jpg" />
+      <div class="image--container">
+        <div>
+          <img src="hero1.png" alt="" />
+        </div>
+        <div>
+          <img src="hero2.png" alt="" />
+        </div>
+        <div>
+          <img src="hero3.png" alt="" />
+        </div>
+        <div>
+          <img src="hero4.png" alt="" />
+        </div>
+        <div>
+          <img src="hero5.png" alt="" />
+        </div>
       </div>
     </section>
   </div>
