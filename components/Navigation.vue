776632024f498f9542e18c0eea75fb4f23eb47c4
Mark Schuurmans <marksmeel@gmail.com>
2022-06-28 18:21:33 +0200

    Added working links

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,11 +1,17 @@
 <template>
   <nav class="nav">
     <ul>
-      <li class="nav__title">Servicez</li>
+      <li class="nav__title">
+        <NuxtLink to="/">Servicez</NuxtLink>
+      </li>
       <div>
-        <li class="nav__item">Over Ons</li>
-        <li class="nav__item">Cocktails</li>
-        <li class="nav__item"><a href="">Nieuws</a></li>
+        <li class="nav__item">
+          <NuxtLink to="/about">Over Ons</NuxtLink>
+        </li>
+        <li class="nav__item">Evenementen</li>
+        <li class="nav__item">
+          <NuxtLink to="/">Nieuws</NuxtLink>
+        </li>
       </div>
       <li class="nav_login"><a href="">Login</a></li>
     </ul>
@@ -17,15 +23,12 @@
   background: rgb(255, 255, 255);
   height: 4rem;
   top: 0;
-  width: 100%;
 
   ul {
     display: flex;
     justify-content: space-between;
     align-items: center;
     height: 100%;
-    width: 80%;
-    margin: 0 auto;
   }
 
   &__title {
