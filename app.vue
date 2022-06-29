8a8c73d5f61808a94c33215e9362f41326a51dab
Markiesch <marksmeel@gmail.com>
2022-06-29 19:31:06 +0200

    Aded favicons

--- app.vue
+++ app.vue
@@ -11,7 +11,13 @@
     <Link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
 
     <!-- Favicon configuration -->
-    <!-- https://realfavicongenerator.net/ -->
+    <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <Link rel="manifest" href="/site.webmanifest" />
+    <Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+    <Meta name="msapplication-TileColor" content="#da532c" />
+    <Meta name="theme-color" content="#ffffff" />
 
     <Meta name="theme-color" content="#ffffff" />
   </Head>
